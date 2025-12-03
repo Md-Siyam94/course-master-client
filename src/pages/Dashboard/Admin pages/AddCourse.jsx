d80873b22f9971a7c06dd5610cabbb3fff22f4a2
Md-Siyam94 <mdsiyam9424@gmail.com>
2025-12-03 19:33:18 +0600

    add course page done

--- src/pages/Dashboard/Admin pages/AddCourse.jsx
+++ src/pages/Dashboard/Admin pages/AddCourse.jsx
@@ -10,22 +10,25 @@ const img_hosting_key = import.meta.env.VITE_img_hosting_key;
 const img_hosting_api = `https://api.imgbb.com/1/upload?key=${img_hosting_key}`
 const AddCourse = () => {
     const axiosSecure = useAxiosSecure()
+    const [error, setError] = useState("")
     const [uploading, setUploading] = useState(false)
     const [modules, setModules] = useState([])
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
 
-    const handleModulesSubmit=(e)=>{
-         e.preventDefault();
+    const handleModulesSubmit = (e) => {
+        e.preventDefault();
 
-          const form = e.target
+        const form = e.target
         const videoTitle = form.videoTitle.value
-        const  videoUrl = form.videoUrl.value
+        const videoUrl = form.videoUrl.value
 
-        setModules(...modules, {videoTitle, videoUrl})
+        setModules([...modules, { videoTitle, videoUrl }])
+        form.reset()
     }
 
     const onSubmit = async (data) => {
+        setError("")
         setUploading(true)
         const imageFile = { image: data?.image[0] }
         // console.log(imageFile);
@@ -44,27 +47,33 @@ const AddCourse = () => {
                 description: data?.description,
                 batch: data?.batch,
                 syllabus: data.syllabus ? data.syllabus.split(",") : [],
-                modules: modules, 
+                modules: modules,
             }
-            console.log(courseInfo);
-            //  axiosSecure.post('/courses', courseInfo)
-            //     .then(res => {
-            //         console.log(res.data);
-            //         if (res?.data?.success) {
-            //             setUploading(false)
-            //             reset()
-            //             Swal.fire({
-            //                 position: "top-end",
-            //                 icon: "success",
-            //                 title: "Product has been uploaded",
-            //                 showConfirmButton: false,
-            //                 timer: 1500
-            //             });
-            //         }
-            //     })
-            //     .catch(err => {
-            //         console.log('error from add product', err);
-            //     })
+            console.log(modules);
+            if (modules.length === 0) {
+                setUploading(false)
+                setError("Please add atleast 1 module")
+                return;
+            }
+            axiosSecure.post('/courses', courseInfo)
+                .then(res => {
+                    console.log(res.data);
+                    if (res?.data?.success) {
+                        setUploading(false)
+                        reset()
+                        setModules([]);
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: "Product has been uploaded",
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                    }
+                })
+                .catch(err => {
+                    console.log('error from add product', err);
+                })
 
         }
     }
@@ -81,7 +90,7 @@ const AddCourse = () => {
                         <label className="label">
                             <span className="label-text">Course Name</span>
                         </label><br />
-                        <input type="text" {...register("title")} placeholder="Course title" className="input input-bordered w-full" />
+                        <input type="text" {...register("title", { required: true })} placeholder="Course title" className="input input-bordered w-full" />
                         <div>
                             {errors.title?.type === 'required' && <p role="alert" className='text-red-600 mt-2'>This field is required !</p>}
                         </div>
@@ -92,7 +101,7 @@ const AddCourse = () => {
                         <label className="label">
                             <span className="label-text">Instructor name</span>
                         </label><br />
-                        <input type="text" {...register("instructor")} placeholder="Instructor name" className="input input-bordered w-full" />
+                        <input type="text" {...register("instructor", { required: true })} placeholder="Instructor name" className="input input-bordered w-full" />
                         <div>
                             {errors.instructor?.type === 'required' && <p role="alert" className='text-red-600 mt-2'>This field is required !</p>}
                         </div>
@@ -102,7 +111,7 @@ const AddCourse = () => {
                         <label className="label">
                             <span className="label-text">Batch no</span>
                         </label><br />
-                        <input type="number" {...register("batch")} placeholder="Batch" className="input input-bordered w-full" />
+                        <input type="number" {...register("batch", { required: true })} placeholder="Batch" className="input input-bordered w-full" />
                         <div>
                             {errors.batch?.type === 'required' && <p role="alert" className='text-red-600 mt-2'>This field is required !</p>}
                         </div>
@@ -112,7 +121,7 @@ const AddCourse = () => {
                         <label className="label">
                             <span className="label-text">Course price</span>
                         </label><br />
-                        <input type="text" {...register("price")} placeholder="Price" className="input input-bordered w-full" />
+                        <input type="text" {...register("price", { required: true })} placeholder="Price" className="input input-bordered w-full" />
                         <div>
                             {errors.price?.type === 'required' && <p role="alert" className='text-red-600 mt-2'>This field is required !</p>}
                         </div>
@@ -122,7 +131,7 @@ const AddCourse = () => {
                         <label className="label">
                             <span className="label-text">Course syllabus</span>
                         </label><br />
-                        <input type="text" {...register("syllabus")} placeholder="Write syllabus " className="input input-bordered w-full" />
+                        <input type="text" {...register("syllabus", { required: true })} placeholder="Write syllabus " className="input input-bordered w-full" />
                         <div>
                             {errors.syllabus?.type === 'required' && <p role="alert" className='text-red-600 mt-2'>This field is required !</p>}
                         </div>
@@ -142,14 +151,15 @@ const AddCourse = () => {
                         <div className="label">
                             <span className="label-text">Description</span>
                         </div><br />
-                        <textarea {...register("description")} className="textarea textarea-bordered h-24 w-full" placeholder="Write about Course" ></textarea>
+                        <textarea {...register("description", { required: true })} className="textarea textarea-bordered h-24 w-full" placeholder="Write about Course" ></textarea>
                         <div>
                             {errors.description?.type === 'required' && <p role="alert" className='text-red-600 mt-2'>This field is required !</p>}
                         </div>
                     </label>
+                    <p className=' text-red-500'>{error}</p>
 
                     <div>
-                        <button className="btn btn-info text-white mt-8">
+                        <button className="btn bg-teal-600 hover:bg-teal-700 py-2 rounded-full text-white mt-8">
                             {
                                 uploading ? "Uploading..." : "Uploade Course"
                             }
@@ -159,26 +169,30 @@ const AddCourse = () => {
             </form>
             <form onSubmit={handleModulesSubmit} className=" bg-base-100 rounded-xl p-10 col-span-5">
                 {/* video title */}
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Video title</span>
-                        </label><br />
-                        <input type="text" name="videoTitle" placeholder="Video title" className="input input-bordered w-full" />
-                        
-                    </div>
-                {/* video url */}
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Youtube embadded video url</span>
-                        </label><br />
-                        <input type="url" name="videoUrl" placeholder="Youtube video url" className="input input-bordered w-full" />
-                        
-                    </div>
+                <div className="form-control my-2">
+                    <label className="label">
+                        <span className="label-text">Video title</span>
+                    </label><br />
+                    <input type="text" name='videoTitle' placeholder="Video title" className="input input-bordered w-full" />
 
-                    <div className="form-control mt-6">
-                        <button className="btn   bg-teal-600 hover:bg-teal-700 text-white">Add modules</button>
-                    </div>
-                
+                </div>
+                {/* video url */}
+                <div className="form-control my-2">
+                    <label className="label">
+                        <span className="label-text">Youtube embadded video url</span>
+                    </label><br />
+                    <input type="url" name="videoUrl" placeholder="Youtube video url" className="input input-bordered w-full" />
+
+                </div>
+
+                <div className="form-control mt-6">
+                    <button className="btn rounded-full  bg-teal-600 hover:bg-teal-700 text-white">Add modules</button>
+                </div>
+
+                <div>
+                    <p className='font-semibold text-lg mt-6'>Total module: {modules.length}</p>
+                </div>
+
             </form>
         </div>
     );
